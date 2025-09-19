34a673795dfae895d9dce5860f3666e7fb155ad3
mmnn0932-admin <nana12030932@gmail.com>
2025-09-20 07:33:39 +0900

    usersテーブル、subjectsテーブルに初期データを登録

--- database/seeders/SubjectsTableSeeder.php
+++ database/seeders/SubjectsTableSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class SubjectsTableSeeder extends Seeder
 {
@@ -14,6 +15,11 @@ class SubjectsTableSeeder extends Seeder
      */
     public function run()
     {
+        DB::table('subjects')->insert([
+            ['subject' => '国語'],
+            ['subject' => '数学'],
+            ['subject' => '英語']
+        ]);
         // 国語、数学、英語を追加
     }
 }
--- database/seeders/UsersTableSeeder.php
+++ database/seeders/UsersTableSeeder.php
@@ -3,6 +3,8 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
 
 class UsersTableSeeder extends Seeder
 {
@@ -13,6 +15,22 @@ class UsersTableSeeder extends Seeder
      */
     public function run()
     {
+        $now = now();
 
+        DB::table('users')->insert([
+            'over_name'         => '山田',
+            'under_name'        => '太郎',
+            'over_name_kana'    => 'ヤマダ',
+            'under_name_kana'   => 'タロウ',
+            'mail_address'      => 'yamada_taro@example.com',
+            'sex'               => 1,
+            'birth_day'         => '2000-01-01',
+            'role'              => 1,
+            'password'          => Hash::make('yamadataro'),
+            'remember_token'    => null,
+            'created_at'        => $now,
+            'updated_at'        => $now,
+            'deleted_at'        => null,
+        ]);
     }
 }

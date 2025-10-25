3b46e8e9faae89bd3f737b9b10880e03762f6833
mmnn0932-admin <nana12030932@gmail.com>
2025-10-26 07:02:15 +0900

    ユーザー情報の表示

--- resources/views/authenticated/calendar/admin/reserve_detail.blade.php
+++ resources/views/authenticated/calendar/admin/reserve_detail.blade.php
@@ -1,17 +1,36 @@
 <x-sidebar>
 <div class="vh-100 d-flex" style="align-items:center; justify-content:center;">
   <div class="w-50 m-auto h-75">
-    <p><span>日</span><span class="ml-3">部</span></p>
+      <p class="mb-3">
+        <span class="ml-2">{{ $date }}</span>
+        <span class="ml-2">{{ [1=>'1部',2=>'2部',3=>'3部'][(int)$part]}}</span>
+      </p>
+
+    @php
+      $setting = $reservePersons->first();
+      $users   = $setting ? $setting->users : collect();
+    @endphp
+
     <div class="h-75 border">
       <table class="">
-        <tr class="text-center">
+      <thead class="text-center">
+        <tr>
           <th class="w-25">ID</th>
           <th class="w-25">名前</th>
+          <th class="w-25">場所</th>
         </tr>
-        <tr class="text-center">
-          <td class="w-25"></td>
-          <td class="w-25"></td>
+      </thead>
+      @if($users->isNotEmpty())
+        <tbody class="text-center">
+        @foreach($users as $user)
+        <tr>
+          <td>{{ $user->id }}</td>
+          <td>{{ $user->over_name }} {{ $user->under_name }}</td>
+          <td>リモート</td>
         </tr>
+      @endforeach
+        </tbody>
+      @endif
       </table>
     </div>
   </div>
